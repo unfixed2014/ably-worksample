0a7503fe8017ec634cf0b6cd35b7301a26dcb68b
KANGHO <kangho@bluehole.net>
2022-08-20 17:42:53 +0900

    refactor: renderWithRouter를 사용한 리팩토링
    https://testing-library.com/docs/example-react-router/#reducing-boilerplate
    
    위에 사이트를 참고해서 헬퍼 함수를 만들고, MemoryRouter를 매번 명시하는
    부분을 수정했습니다.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,46 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
 import App from './App';
-import { MemoryRouter } from 'react-router';
+import renderWithRouter from './__test__/utils/renderWithRouter';
 
 test('App 컴포넌트가 렌더링 외어야 한다', () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  renderWithRouter(<App />);
   expect(screen.getByAltText('logo')).toBeInTheDocument();
 });
 
 test('Home으로 가는 Link가 렌더링 되어야 한다', () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-
+  renderWithRouter(<App />);
   const linkElement = screen.getByText('Home');
   expect(linkElement).toBeInTheDocument();
 });
 
 test('Home으로 가는 Link를 클릭하면 Home으로 이동한다', async () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  const { user } = renderWithRouter(<App />);
 
-  const user = userEvent.setup();
   await user.click(screen.getByText('Home'));
   expect(screen.getByText('homepage')).toBeInTheDocument();
 });
 
 test('Router에 등록되지 않는 페이지가 나오면 404가 노출되어야 한다', () => {
-  render(
-    <MemoryRouter initialEntries={['/pwoiejrioq']}>
-      <App />
-    </MemoryRouter>,
-  );
-
+  renderWithRouter(<App />, { initialEntries: ['/not-found'] });
   expect(screen.getByText('404')).toBeInTheDocument();
+});
+
+test('Abount 페이지로 처음에 진입하면 About 페이지가 노출되어야 한다', () => {
+  renderWithRouter(<App />, { initialEntries: ['/about'] });
+  expect(screen.getByText('aboutpage')).toBeInTheDocument();
 });
---
+++ src/__test__/utils/renderWithRouter.tsx
@@ -0,0 +1,14 @@
+import userEvent from '@testing-library/user-event';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import { ReactElement } from 'react';
+
+const renderWithRouter = (ui: ReactElement, { initialEntries = ['/'] } = {}) => {
+  return {
+    user: userEvent.setup(),
+
+    ...render(<MemoryRouter initialEntries={initialEntries}>{ui}</MemoryRouter>),
+  };
+};
+
+export default renderWithRouter;
