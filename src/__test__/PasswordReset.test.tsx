d7db79d89f218ee4f37504712c09b562c6d2611e
KANGHO <kangho@bluehole.net>
2022-08-21 19:07:39 +0900

    feature: 패스워드 초기화 페이지에서 form 추가

---
+++ src/__test__/PasswordReset.test.tsx
@@ -0,0 +1,34 @@
+// - [x] 이메일을 입력 할 수 있는 Input Form과 다음(next) Button을 배치합니다.
+// - [] 다음 Button을 클릭하면 이메일을 검증합니다.
+// - [] [4. 인증 코드 발급 요청 API] 를 호출하고 응답 결과에 따라 처리합니다.
+// - [] 호출에 실패하면 메시지로 알립니다.
+// - [] 호출이 성공하면 [B. 인증 코드 검증 페이지] 로 이동합니다.
+
+import { screen } from '@testing-library/dom';
+import PasswordReset from '../pages/PasswordReset';
+import { AuthService, FakeAuthService } from '../_lib/AuthServices';
+import { defaultProvider, DepsProvider } from '../_lib/DepContext';
+import renderWithRouter from './utils/renderWithRouter';
+
+const PasswordResetWithDep = (providers = {}) => (
+  <DepsProvider
+    services={{
+      ...defaultProvider,
+      ...providers,
+    }}
+  >
+    <PasswordReset />
+  </DepsProvider>
+);
+
+test('컴포넌트가 렌더링 되어야 한다.', async () => {
+  renderWithRouter(<PasswordReset />);
+  expect(screen.getByTestId('passwordResetWapper')).toBeInTheDocument();
+});
+
+test('email 입력이 가능한 input버튼과 다음 버튼이 렌더링 되어야 한다', () => {
+  renderWithRouter(<PasswordReset />);
+
+  expect(screen.getByTestId('emailInput')).toBeInTheDocument();
+  expect(screen.getByTestId('nextBtn')).toBeInTheDocument();
+});
--- src/pages/PasswordReset.tsx
+++ src/pages/PasswordReset.tsx
@@ -1,5 +1,12 @@
 const PasswordReset = () => {
-  return <div data-testid="passwordResetWapper"></div>;
+  return (
+    <div data-testid="passwordResetWapper">
+      <form>
+        <input type="email" placeholder="email" data-testid="emailInput" />
+        <button type="submit" value="다음" data-testid="nextBtn" />
+      </form>
+    </div>
+  );
 };
 
 export default PasswordReset;
