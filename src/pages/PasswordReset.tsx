fc1097c544d3d7bf16e6c8dca0db085da0f596ae
KANGHO <kangho@bluehole.net>
2022-08-21 21:12:08 +0900

    refactor: useNavigate hook 사용해서 네비게이션 하도록 변경

--- src/pages/PasswordReset.tsx
+++ src/pages/PasswordReset.tsx
@@ -1,24 +1,22 @@
 import { FormEvent, useState } from 'react';
 import { useDeps } from '../_lib/DepContext';
-import { Navigate } from 'react-router-dom';
-import { requestEmailVerificationResponse } from '../_lib/AuthServices';
+import { useNavigate } from 'react-router-dom';
 
 const PasswordReset = () => {
   const { authService } = useDeps();
   const [email, setEmail] = useState('');
-  const [isRequestSuccess, setIsRequestSuccess] = useState(false);
   const [errorMessage, setErrorMessage] = useState();
-  const [verifyReponse, setVerifyReponse] = useState<requestEmailVerificationResponse | null>(null);
+  const navigate = useNavigate();
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     if (!email) {
       return;
     }
+
     try {
-      const ret = await authService.requestEmailVerification(email);
-      setVerifyReponse(ret);
-      setIsRequestSuccess(true);
+      const state = await authService.requestEmailVerification(email);
+      navigate('/verify-code', { replace: true, state });
     } catch (err: any) {
       setErrorMessage(err.message);
       console.log(err);
@@ -27,7 +25,6 @@ const PasswordReset = () => {
 
   return (
     <>
-      {isRequestSuccess && <Navigate to="/verify-code" replace={true} state={{ verifyReponse }} />}
       <div data-testid="passwordResetWapper">
         {errorMessage && <div data-testid="errorMessage">{errorMessage}</div>}
         <form onSubmit={handleSubmit}>
