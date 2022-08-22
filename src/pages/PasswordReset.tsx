f49f9f738ce1f2f8acff24d37e1a1aede7bcc178
KANGHO <kangho@bluehole.net>
2022-08-23 00:02:29 +0900

    style: 비밀번호 초기화 페이지에서 tailwindcss 적용

--- src/pages/PasswordReset.tsx
+++ src/pages/PasswordReset.tsx
@@ -34,22 +34,43 @@ const PasswordReset = () => {
   };
 
   return (
-    <>
-      <div data-testid="passwordResetWapper">
+    <div
+      className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
+      data-testid="passwordResetWapper"
+    >
+      <div className="max-w-md w-full space-y-8">
         {errorMessage && <div data-testid="errorMessage">{errorMessage}</div>}
         <form onSubmit={handleSubmit}>
-          <input
-            type="email"
-            placeholder="email"
-            autoFocus
-            data-testid="emailInput"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input type="submit" value="다음" data-testid="nextBtn" />
+          <div className="shadow overflow-hidden sm:rounded-md">
+            <div className="px-4 py-5 bg-white sm:p-6">
+              <div className="col-span-6 sm:col-span-3">
+                <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                  이메일
+                </label>
+                <input
+                  type="email"
+                  placeholder="이메일"
+                  autoFocus
+                  data-testid="emailInput"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+              <button
+                type="submit"
+                className="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                data-testid="nextBtn"
+              >
+                다음
+              </button>
+            </div>
+          </div>
         </form>
       </div>
-    </>
+    </div>
   );
 };
 
