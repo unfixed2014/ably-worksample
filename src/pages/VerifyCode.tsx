55f4c75d3684ba7d427d9de265b0cb1eb164d7a9
KANGHO <kangho@bluehole.net>
2022-08-23 03:10:22 +0900

    squash

--- src/pages/VerifyCode.tsx
+++ src/pages/VerifyCode.tsx
@@ -30,19 +30,32 @@ const VerifyCode = () => {
     return () => clearInterval(timerId.current);
   }, []);
 
-  const handleSubmit = async (e: FormEvent) => {
+  const padding = (num: number) => {
+    if (num < 10) {
+      return `0${num}`;
+    }
+    return num;
+  };
+
+  const handleSubmit = async (e: FormEvent, timeInSecond: number) => {
     e.preventDefault();
+
+    const { email, issueToken } = state as {
+      email: string;
+      issueToken: string;
+    };
 
     if (!authCode) {
       setError('인증 코드를 입력해주세요.');
       return;
     }
 
+    if (timeInSecond === 0) {
+      setError('인증 시간이 초과되었습니다.');
+      return;
+    }
+
     try {
-      const { email, issueToken } = state as {
-        email: string;
-        issueToken: string;
-      };
       const { confirmToken } = await authService.requestVerifyCode(email, authCode, issueToken);
       navigate('/modify-password', { state: { email, confirmToken } });
     } catch (err) {
@@ -62,18 +75,9 @@ const VerifyCode = () => {
     );
   }
 
-  const padding = (num: number) => {
-    if (num < 10) {
-      return `0${num}`;
-    }
-    return num;
-  };
-
   const timeInSecond = Math.floor((state as any).remainMillisecond) / 1000 - count;
   const displaySecond = padding(Math.floor(timeInSecond % 60));
   const displayMinute = padding(Math.floor(timeInSecond / 60));
-
-  console.log(timerId.current);
 
   if (timeInSecond === 0) {
     window.clearInterval(timerId.current);
@@ -89,7 +93,7 @@ const VerifyCode = () => {
           {displayMinute}:{displaySecond}
         </p>
         {errorMessage && <div data-testid="errorMessage">{errorMessage}</div>}
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={(e) => handleSubmit(e, timeInSecond)}>
           <div className="shadow overflow-hidden sm:rounded-md">
             <div className="px-4 py-5 bg-white sm:p-6">
               <div className="col-span-6 sm:col-span-3">
