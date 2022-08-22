613a0af38cad5e3e726fd065520b334f5a5172c8
KANGHO <kangho@bluehole.net>
2022-08-23 00:07:02 +0900

    style: 인증 코드 입력하는 페이지 스타일링 작업

--- src/pages/VerifyCode.tsx
+++ src/pages/VerifyCode.tsx
@@ -44,20 +44,43 @@ const VerifyCode = () => {
   }
 
   return (
-    <div data-testid="verifyCodeWrapper">
-      <p data-testid="counter">00:00</p>
-      <p data-testid="errorMessage">{errorMessage}</p>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="인증 코드를 입력하세요"
-          autoFocus
-          data-testid="verifyCodeInput"
-          value={verifyCode}
-          onChange={(e) => setVerifyCode(e.target.value)}
-        />
-        <input type="submit" value="다음" data-testid="nextBtn" />
-      </form>
+    <div
+      data-testid="verifyCodeWrapper"
+      className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
+    >
+      <div className="max-w-md w-full space-y-8">
+        <p data-testid="counter">00:00</p>
+        {errorMessage && <div data-testid="errorMessage">{errorMessage}</div>}
+        <form onSubmit={handleSubmit}>
+          <div className="shadow overflow-hidden sm:rounded-md">
+            <div className="px-4 py-5 bg-white sm:p-6">
+              <div className="col-span-6 sm:col-span-3">
+                <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                  인증 코드
+                </label>
+                <input
+                  type="text"
+                  placeholder="인증 코드를 입력하세요"
+                  autoFocus
+                  data-testid="verifyCodeInput"
+                  value={verifyCode}
+                  onChange={(e) => setVerifyCode(e.target.value)}
+                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+              <button
+                type="submit"
+                className="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                data-testid="nextBtn"
+              >
+                다음
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
