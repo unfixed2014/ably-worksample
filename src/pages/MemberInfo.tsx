7cbf1884308df1240544157e81dc9d9a390a5597
KANGHO <kangho@bluehole.net>
2022-08-23 02:21:09 +0900

    style: 멤버 정보 페이지 스타일 수정

--- src/pages/MemberInfo.tsx
+++ src/pages/MemberInfo.tsx
@@ -38,22 +38,37 @@ const MemberInfo = () => {
   };
 
   return (
-    <>
-      <div data-testid="memberInfoWrapper">
-        <h1>Member Info</h1>
-        {memberInfo && (
-          <div data-testid="memberInfoCard">
-            <p data-testid="memberName">{memberInfo.name}</p>
-            <p>{memberInfo.email}</p>
-            <img src={memberInfo.profileImage} />
+    <div
+      data-testid="memberInfoWrapper"
+      className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
+    >
+      <div className="max-w-md w-full space-y-8">
+        <div className="shadow overflow-hidden sm:rounded-md">
+          <div className="col-span-6 sm:col-span-3">
+            <div className="px-4 py-5 bg-white sm:p-6">
+              {memberInfo && (
+                <div data-testid="memberInfoCard">
+                  <p data-testid="memberName">{memberInfo.name}</p>
+                  <p>{memberInfo.email}</p>
+                  <img src={memberInfo.profileImage} />
+                </div>
+              )}
+              {errorMessage && <p data-testid="errorMessage">{errorMessage}</p>}
+            </div>
           </div>
-        )}
-        {errorMessage && <p data-testid="errorMessage">{errorMessage}</p>}
-        <button type="button" data-testid="logoutBtn" onClick={handleClickLogout}>
-          로그아웃
-        </button>
+          <div className="py-3 bg-gray-50 text-right sm:px-6">
+            <button
+              type="button"
+              data-testid="logoutBtn"
+              className="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={handleClickLogout}
+            >
+              로그아웃
+            </button>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
