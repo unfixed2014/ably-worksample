f8a42a718e5d0db830c34d0991b8d3bb1fa72b16
KANGHO <kangho@bluehole.net>
2022-08-22 09:16:47 +0900

    refactor: renderWithRouter  타입 추가

--- src/__test__/utils/renderWithRouter.tsx
+++ src/__test__/utils/renderWithRouter.tsx
@@ -2,8 +2,12 @@ import userEvent from '@testing-library/user-event';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import { ReactElement } from 'react';
+import { InitialEntry } from 'history';
 
-const renderWithRouter = (ui: ReactElement, { initialEntries = ['/'] } = {}) => {
+const renderWithRouter = (
+  ui: ReactElement,
+  { initialEntries }: { initialEntries: InitialEntry[] } = { initialEntries: ['/'] },
+) => {
   return {
     user: userEvent.setup(),
     ...render(<MemoryRouter initialEntries={initialEntries}>{ui}</MemoryRouter>),
